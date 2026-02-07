7cd8c3441b4087374db8b05e707d94c2fdafef19
opendev-labs <yashramteke55555@gmail.com>
2026-02-07 02:48:47 +0000

    Deploying to gh-pages from @ opendev-labs/opendev-labs.github.io@49b809cf40063d673cb9390390a379ea02e1570b 🚀

--- assets/CLIPage-DBme2bC0.js
+++ assets/CLIPage-DBme2bC0.js
@@ -1,4 +1,4 @@
-import{u as H,r as i,j as n,b as L}from"./index-BgMjOyQY.js";import{b as U,s as I}from"./VoidApp-l64KEhNI.js";const _=`
+import{u as H,r as i,j as n,b as L}from"./index-JugZuJZC.js";import{b as U,s as I}from"./VoidApp-DAJ6kbKM.js";const _=`
 <div class="space-y-1">
 <div class="font-bold text-white uppercase tracking-widest text-[10px] mb-2">opendev CLI - Operational Protocols</div>
 <div class="pl-0 flex gap-4"><span class="text-white font-bold w-32 shrink-0">help</span> <span class="text-zinc-600">Display available protocols.</span></div>
